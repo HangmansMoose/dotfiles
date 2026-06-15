3af809bc02de29bcf0db5039a913a495cbbb2808
HangmansMoose <trivial86@protonmail.com>
2026-06-15 10:54:35 +1000

    updated vscode files and odded misc

--- dots_setup.ps1
+++ dots_setup.ps1
@@ -4,57 +4,28 @@
 #Requires -Version 7
 
 # Linked Files (Destination => Source)
-$symlinks = @{
-    $PROFILE.CurrentUserAllHosts                                                                    = ".\Profile.ps1"
-    "$HOME\AppData\Local\nvim"                                                                      = ".\nvim"
-    "$HOME\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" = ".\windowsterminal\settings.json"
-    "$HOME\.gitconfig"                                                                              = ".\.gitconfig"
-    "$HOME\AppData\Roaming\Code\User\settings.json"                                                 = ".\vscode\settings.json"
-    "$HOME\.config\wezterm"                                                                         = ".\wezterm"
-    "$ENV:PROGRAMFILES\WezTerm\wezterm_modules"                                                     = ".\wezterm\"
-}
+#$symlinks = @{
+#    $PROFILE.CurrentUserAllHosts                                                                    = ".\Profile.ps1"
+#    "$HOME\AppData\Local\nvim"                                                                      = ".\nvim"
+#    "$HOME\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json" = ".\windowsterminal\settings.json"
+#    "$HOME\.gitconfig"                                                                              = ".\.gitconfig"
+#    "$HOME\AppData\Roaming\Code\User\settings.json"                                                 = ".\vscode\settings.json"
+#    "$HOME\.config\wezterm"                                                                         = ".\wezterm"
+#    "$ENV:PROGRAMFILES\WezTerm\wezterm_modules"                                                     = ".\wezterm\"
+#}
 
 # Winget & choco dependencies
 $wingetDeps = @(
-    "chocolatey.chocolatey"
-    "eza-community.eza"
-    "ezwinports.make"
+    "microsoft.powershell"
+	"microsoft.coreutils"
     "git.git"
     "github.cli"
     "kitware.cmake"
-    "mbuilov.sed"
-    "microsoft.powershell"
     "Microsoft.VisualStudioCode"
     "Notepad++.Notepad++"
     "neovim.neovim"
-    "starship.starship"
-    "task.task"
+	"odin-lang.odin"
 )
-$chocoDeps = @(
-    "bat"
-    "fd"
-    "fzf"
-    "gawk"
-    "lazygit"
-    "mingw"
-    "oh-my-posh"
-    "nerd-fonts-CascadiaMono"
-    "nerd-fonts-FiraMono"
-    "nerd-fonts-LiberationMono"
-    "nerd-fonts-FiraCode"
-    "nerd-fonts-JetBrainsMono"
-    "ripgrep"
-    "wezterm"
-    "zig"
-    "zoxide"
-)
-
-# PS Modules
-#$psModules = @(
-#    "CompletionPredictor"
-#    "PSScriptAnalyzer"
-#    "ps-color-scripts"
-#)
 
 # Set working directory
 Set-Location $PSScriptRoot
@@ -68,66 +39,6 @@ foreach ($wingetDep in $wingetDeps) {
     }
 }
 
-# Path Refresh
-$env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
-
-$installedChocoDeps = (choco list --limit-output --id-only).Split("`n")
-foreach ($chocoDep in $chocoDeps) {
-    if ($installedChocoDeps -notcontains $chocoDep) {
-        choco install $chocoDep -y
-    }
-}
-
-# Install PS Modules
-#foreach ($psModule in $psModules) {
-#    if (!(Get-Module -ListAvailable -Name $psModule)) {
-#        Install-Module -Name $psModule -Force -AcceptLicense -Scope CurrentUser
-#    }
-#}
-
-# Delete OOTB Nvim Shortcuts (including QT)
-if (Test-Path "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Neovim\") {
-    Remove-Item "$env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Neovim\" -Recurse -Force
-}
-
-# Persist Environment Variables
-[System.Environment]::SetEnvironmentVariable('WEZTERM_CONFIG_FILE', "$PSScriptRoot\wezterm\wezterm.lua", [System.EnvironmentVariableTarget]::User)
-
-$currentGitEmail = (git config --global user.email)
-$currentGitName = (git config --global user.name)
-
-mkdir "$HOME\.config"
-
-# Create Symbolic Links
-Write-Host "Creating Symbolic Links..."
-foreach ($symlink in $symlinks.GetEnumerator()) {
-    Get-Item -Path $symlink.Key -ErrorAction SilentlyContinue | Remove-Item -Force -Recurse -ErrorAction SilentlyContinue
-    New-Item -ItemType SymbolicLink -Path $symlink.Key -Target (Resolve-Path $symlink.Value) -Force | Out-Null
-}
-
-git config --global --unset user.email | Out-Null
-git config --global --unset user.name | Out-Null
-git config --global user.email $currentGitEmail | Out-Null
-git config --global user.name $currentGitName | Out-Null
-
-# Install bat themes
-bat cache --clear
-bat cache --build
-
-# Clone zvm
-Write-Host "Grabbing ZVM"
-$zvmUrl = "https://github.com/tristanisham/zvm.git"
-$zvmTarget = "$HOME\.zvm"
-
-git clone $zvmUrl $zvmTarget
-
 # Refresh Environment Variables
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
-
-Write-Host "Installing Zig 0.15.1 and Master"
-zvm install --zls "0.15.1"
-zvm install --zls "master"
-
-Write-Host "Setting Zig 0.15.1 as use version"
-zvm use "0.15.1"
 
