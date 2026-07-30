0de26686ddb8ee61bf196535e26203c11f8af14c
HangmansMoose <trivial86@protonmail.com>
2026-07-31 08:24:44 +1000

    added filepilot

--- profile.ps1
+++ profile.ps1
@@ -1,80 +1,42 @@
-# Import the Chocolatey Profile that contains the necessary code to enable
-# tab-completions to function for `choco`.
-# Be aware that if you are missing these lines from your profile, tab completion
-# for `choco` will not function.
-# See https://ch0.co/tab-completion for details.
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
-}
-
 $PSStyle.FileInfo.Directory = ""
 
-oh-my-posh --init --shell pwsh --config C:\Users\glens\appdata\local\programs\oh-my-posh\themes\negligible.omp.json | Invoke-Expression
+#Invoke-Expression (&starship init powershell)
+oh-my-posh init pwsh --config "negligible" | Invoke-Expression
 
+#Set-Alias -Name ls -Value Get-ChildItemPretty
+#Set-Alias -Name rm -Value Remove-ItemExtended
+#Set-Alias -Name touch -Value New-File
+#Set-Alias -Name which -Value Show-Command
+Set-Alias -Name nvd -Value neovide
 
-
-# Aliases 🔗
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-#Set-Alias -Name cat -Value bat
-Set-Alias -Name df -Value Get-Volume
-Set-Alias -Name ff -Value Find-File
-Set-Alias -Name grep -Value Find-String
-Set-Alias -Name l -Value Get-ChildItemPretty
-Set-Alias -Name la -Value Get-ChildItemPretty
-Set-Alias -Name ll -Value Get-ChildItemPretty
-Set-Alias -Name ls -Value Get-ChildItemPretty
-Set-Alias -Name rm -Value Remove-ItemExtended
-Set-Alias -Name su -Value Update-ShellElevation
-Set-Alias -Name touch -Value New-File
-Set-Alias -Name which -Value Show-Command
-
-function Show-Command {
+function Update-Profile {
     <#
     .SYNOPSIS
-        Displays the definition of a command. Alias: which
+        Gets the latest changes from git, reruns the setup script and reloads the profile.
+        Note that functions won't be updated, this requires a full PS session restart. Alias: up
     #>
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory = $true, Position = 0)]
-        [string]$Name
-    )
-    Write-Verbose "Showing definition of '$Name'"
-    Get-Command $Name | Select-Object -ExpandProperty Definition
-}
+    Write-Verbose "Storing current working directory in memory"
+    $currentWorkingDirectory = $PWD
 
-function Remove-ItemExtended {
-    <#
-    .SYNOPSIS
-        Removes an item and (optionally) all its children. Alias: rm
-    #>
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory = $false)]
-        [switch]$rf,
-        [Parameter(Mandatory = $true, Position = 0)]
-        [string]$Path
-    )
+    Write-Verbose "Updating local profile from Github repository"
+    Set-Location $ENV:WindotsLocalRepo
+    git stash | Out-Null
+    git pull | Out-Null
+    git stash pop | Out-Null
 
-    Write-Verbose "Removing item '$Path' $($rf ? 'and all its children' : '')"
-    Remove-Item $Path -Recurse:$rf -Force:$rf
-}
+    Write-Verbose "Rerunning setup script to capture any new dependencies."
+    if (Get-Command -Name sudo -ErrorAction SilentlyContinue) {
+        sudo pwsh ./Setup.ps1
+    }
+    else {
+        Start-Process wezterm -Verb runAs -WindowStyle Hidden -ArgumentList "start --cwd $PWD pwsh -NonInteractive -Command ./Setup.ps1"
+    }
 
-function Get-ChildItemPretty {
-    <#
-    .SYNOPSIS
-        Runs eza with a specific set of arguments. Plus some line breaks before and after the output.
-        Alias: ls, ll, la, l
-    #>
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory = $false, Position = 0)]
-        [string]$Path = $PWD
-    )
+    Write-Verbose "Reverting to previous working directory"
+    Set-Location $currentWorkingDirectory
 
-    Write-Host ""
-    eza -a -l --header --icons --hyperlink --time-style relative $Path
-    Write-Host ""
+    Write-Verbose "Re-running profile script from $($PROFILE.CurrentUserAllHosts)"
+    .$PROFILE.CurrentUserAllHosts
 }
 
 function Update-Software {
@@ -115,10 +77,127 @@ function Update-ShellElevation {
     sudo -E pwsh -NoLogo -Interactive -NoExit -c "Clear-Host"
 }
 
+function Find-String {
+    <#
+    .SYNOPSIS
+        Searches for a string in a file or directory. Alias: grep
+    #>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true, Position = 0)]
+        [string]$SearchTerm,
+        [Parameter(ValueFromPipeline, Mandatory = $false, Position = 1)]
+        [string]$Directory,
+        [Parameter(Mandatory = $false)]
+        [switch]$Recurse
+    )
+
+    Write-Verbose "Searching for '$SearchTerm' in '$Directory'"
+    if ($Directory) {
+        if ($Recurse) {
+            Write-Verbose "Searching for '$SearchTerm' in '$Directory' and subdirectories"
+            Get-ChildItem -Recurse $Directory | Select-String $SearchTerm
+            return
+        }
+
+        Write-Verbose "Searching for '$SearchTerm' in '$Directory'"
+        Get-ChildItem $Directory | Select-String $SearchTerm
+        return
+    }
+
+    if ($Recurse) {
+        Write-Verbose "Searching for '$SearchTerm' in current directory and subdirectories"
+        Get-ChildItem -Recurse | Select-String $SearchTerm
+        return
+    }
+
+    Write-Verbose "Searching for '$SearchTerm' in current directory"
+    Get-ChildItem | Select-String $SearchTerm
+}
+
+function New-File {
+    <#
+    .SYNOPSIS
+        Creates a new file with the specified name and extension. Alias: touch
+    #>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true, Position = 0)]
+        [string]$Name
+    )
+
+    Write-Verbose "Creating new file '$Name'"
+    New-Item -ItemType File -Name $Name -Path $PWD | Out-Null
+}
+
+function Show-Command {
+    <#
+    .SYNOPSIS
+        Displays the definition of a command. Alias: which
+    #>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true, Position = 0)]
+        [string]$Name
+    )
+    Write-Verbose "Showing definition of '$Name'"
+    Get-Command $Name | Select-Object -ExpandProperty Definition
+}
+
+function Get-ChildItemPretty {
+    <#
+    .SYNOPSIS
+        Runs eza with a specific set of arguments. Plus some line breaks before and after the output.
+        Alias: ls, ll, la, l
+    #>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $false, Position = 0)]
+        [string]$Path = $PWD
+    )
+
+    Write-Host ""
+    eza -a -l --header --icons --hyperlink --time-style relative $Path
+    Write-Host ""
+}
+
+function Remove-ItemExtended {
+    <#
+    .SYNOPSIS
+        Removes an item and (optionally) all its children. Alias: rm
+    #>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $false)]
+        [switch]$rf,
+        [Parameter(Mandatory = $true, Position = 0)]
+        [string]$Path
+    )
+
+    Write-Verbose "Removing item '$Path' $($rf ? 'and all its children' : '')"
+    Remove-Item $Path -Recurse:$rf -Force:$rf
+}
+
+
 # Environment Variables 🌐
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 $ENV:BAT_CONFIG_DIR = "$ENV:WindotsLocalRepo\bat"
 $ENV:FZF_DEFAULT_OPTS = '--color=fg:-1,fg+:#ffffff,bg:-1,bg+:#3c4048 --color=hl:#5ea1ff,hl+:#5ef1ff,info:#ffbd5e,marker:#5eff6c --color=prompt:#ff5ef1,spinner:#bd5eff,pointer:#ff5ea0,header:#5eff6c --color=gutter:-1,border:#3c4048,scrollbar:#7b8496,label:#7b8496 --color=query:#ffffff --border="rounded" --border-label="" --preview-window="border-rounded" --height 40% --preview="bat -n --color=always {}"'
+
+
+#Invoke-Expression (& { ( zoxide init powershell --cmd cd | Out-String ) })
+
+Import-Module -Name Terminal-Icons
+
+$colors = @{
+    "Operator"         = "`e[35m" # Purple
+    "Parameter"        = "`e[36m" # Cyan
+    "String"           = "`e[32m" # Green
+    "Command"          = "`e[34m" # Blue
+    "Variable"         = "`e[37m" # White
+    "Comment"          = "`e[38;5;244m" # Gray
+    "InlinePrediction" = "`e[38;5;244m" # Gray
+}
 
 Set-PSReadLineOption -Colors $colors
 Set-PSReadLineOption -PredictionSource HistoryAndPlugin
@@ -126,7 +205,8 @@ Set-PSReadLineOption -PredictionViewStyle InlineView
 Set-PSReadLineKeyHandler -Function AcceptSuggestion -Key Alt+l
 Import-Module -Name CompletionPredictor
 
-# Find all Launch-VsDevShell.ps1 on the system
-$vswhere = "${env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe"
-$vsPath = & $vswhere -latest -property installationPath
-& "$vsPath\Common7\Tools\Launch-VsDevShell.ps1" -Arch amd64 -SkipAutomaticLocation
+# Skip fastfetch for non-interactive shells
+if ([Environment]::GetCommandLineArgs().Contains("-NonInteractive")) {
+    return
+}
+. G:\dev_tools\QSetup\qsetup.ps1
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,50 +2,70 @@
 local wezterm = require("wezterm")
 local launch_menu = {}
 local config = wezterm.config_builder()
+local act = wezterm.action
 
 -- This is where you actually apply your config choices
 
 --- Get the current operating system
 --- @return "windows"| "linux" | "macos"
+--local function get_os()
+--    local bin_format = package.cpath:match("%p[\\|/]?%p(%a+)")
+--    if bin_format == "dll" then
+--        return "windows"
+--    elseif bin_format == "so" then
+--        return "linux"
+--    end
+--
+--    return "macos"
+--end
+
 local function get_os()
-	local bin_format = package.cpath:match("%p[\\|/]?%p(%a+)")
-	if bin_format == "dll" then
-		return "windows"
-	elseif bin_format == "so" then
-		return "linux"
-	end
-
-	return "macos"
+    local cpath = package.cpath
+    if cpath:match("%.dll") then
+        return "Windows"
+    elseif cpath:match("%.dylib") then
+        return "macOS"
+    else
+        return "Linux"
+    end
 end
-
-local isWindows = package.config:sub(1, 1) == "\\"   
 
 local host_os = get_os()
 
 -- For example, changing the color scheme:
-config.color_scheme = "Black Metal (Mayhem)(base16)"
-if isWindows then
-	config.default_cwd = "G:/"
+config.color_scheme = "Tomorrow Night Bright"
+if host_os == 'Windows' then
+    config.default_cwd = "G:/"
 else
-	config.default_cwd = "~"
+    config.default_cwd = "~/dev"
 end
 config.font = wezterm.font("CaskaydiaMono Nerd Font")
 config.font_size = 14
-config.animation_fps = 60
+config.animation_fps = 144
 config.default_cursor_style = "BlinkingBlock"
 config.cursor_blink_ease_in = "Constant"
 config.cursor_blink_ease_out = "Constant"
-config.cursor_blink_rate = 750
+config.cursor_blink_rate = 500
 config.scrollback_lines = 20000
 config.automatically_reload_config = true
-config.max_fps = 60
+config.max_fps = 120
+config.term = "xterm-256"
 -- WebGpu allows for the use of DirectX in windows
-config.front_end = "OpenGL"
-config.allow_win32_input_mode = true
+config.webgpu_preferred_adapter = {
+        backend = "Dx12",
+        device = 9373,
+        device_type = "DiscreteGpu",
+        driver = "32.0.16.1074",
+        name = "NVIDIA GeForce RTX 3070 Laptop GPU",
+        vendor = 4318,
+}
+config.front_end = "WebGpu"
 
 config.colors = {
-	cursor_bg = "#00ff33",
-	cursor_fg = "#303030",
+}
+
+config.window_frame = {
+	active_titlebar_bg = "#050505"
 }
 config.default_prog = {
 	"pwsh.exe",
@@ -54,10 +74,7 @@ config.default_prog = {
 }
 
 config.launch_menu = {
-	{ label = "Powershell", args = { "pwsh.exe", "-NoLogo", "-NoExit" } },
-	{ label = "Powershell VSDev", args = { "pwsh.exe", "-NoLogo", "-NoExit", "-File", "G:/dev_tools/vsdev.ps1" } },
-	{ label = "Arch", args = { "wsl.exe", "-d", "archlinux" } },
-	{ label = "Ubuntu", args = { "wsl.exe", "-d", "Ubuntu-26.04" } },
+    { label = "Powershell Core", args = { "pwsh.exe", "-NoLogo", "-NoExit" } },
 }
 
 -- This section makes wezterm launch into fullscreen mode
@@ -73,16 +90,62 @@ require("wezterm").on("format-window-title", function()
 	return "Wezterm"
 end)
 
-config.enable_tab_bar = false
+config.window_decorations = "NONE | RESIZE"
+config.enable_tab_bar = true
 config.hide_tab_bar_if_only_one_tab = true
-config.use_fancy_tab_bar = true
+config.use_fancy_tab_bar = false
+-- config.tab_bar_at_bottom = true
+config.colors = {
+	-- background = '#3b224c',
+	-- background = "#181616", -- vague.nvim bg
+	-- background = "#080808", -- almost black
+	-- background = "#0c0b0f", -- dark purple
+	-- background = "#020202", -- dark purple
+	-- background = "#17151c", -- brighter purple
+	-- background = "#16141a",
+	-- background = "#0e0e12", -- bright washed lavendar
+	-- background = 'rgba(59, 34, 76, 100%)',
+	-- cursor_border = "#bea3c7",
+	-- cursor_fg = "#281733",
+	-- cursor_bg = "#bea3c7",
+	-- selection_fg = '#281733',
+	cursor_bg = "#7e7e7e",
+	cursor_fg = "#1a1a1a",
+
+	tab_bar = {
+		background = "#0c0b0f",
+		-- background = "rgba(0, 0, 0, 0%)",
+		active_tab = {
+			bg_color = "#0c0b0f",
+			fg_color = "#FFA717",
+			intensity = "Normal",
+			underline = "None",
+			italic = false,
+			strikethrough = false,
+		},
+		inactive_tab = {
+			bg_color = "#0c0b0f",
+			fg_color = "#f8f2f5",
+			intensity = "Normal",
+			underline = "None",
+			italic = false,
+			strikethrough = false,
+		},
+
+		new_tab = {
+			-- bg_color = "rgba(59, 34, 76, 50%)",
+			bg_color = "#0c0b0f",
+			fg_color = "white",
+		},
+	},
+}
 
 config.leader = { key = "a", mods = "CTRL" }
 config.keys = {
-	--	Using the ShowLauncherArgs command with the LAUNCH_MENU_ITEMS flag is what allows you to show only what you have defined
-	--	in config.launch_menu in the launcher
-	{ key = "F3", mods = "NONE", action = wezterm.action.ShowLauncherArgs({ flags = "LAUNCH_MENU_ITEMS" }) },
-	{ key = "F2", mods = "NONE", action = wezterm.action.ActivateCommandPalette },
+    --	Using the ShowLauncherArgs command with the LAUNCH_MENU_ITEMS flag is what allows you to show only what you have defined
+    --	in config.launch_menu in the launcher
+    { key = 'F3', mods = 'NONE', action = wezterm.action.ShowLauncherArgs{ flags = 'LAUNCH_MENU_ITEMS' } },
+    { key = 'F2', mods = 'NONE', action = wezterm.action.ActivateCommandPalette },
 	{ key = "-", mods = "LEADER", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
 	{ key = "%", mods = "LEADER|SHIFT", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
 	{ key = "s", mods = "LEADER", action = wezterm.action({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
@@ -110,6 +173,63 @@ config.keys = {
 	{ key = "&", mods = "LEADER|SHIFT", action = wezterm.action({ CloseCurrentTab = { confirm = true } }) },
 	{ key = "d", mods = "LEADER", action = wezterm.action({ CloseCurrentPane = { confirm = true } }) },
 	{ key = "x", mods = "LEADER", action = wezterm.action({ CloseCurrentPane = { confirm = true } }) },
+	{
+		key = "E",
+		mods = "CTRL|SHIFT|ALT",
+		action = wezterm.action.EmitEvent("toggle-colorscheme"),
+	},
+	{
+		key = "h",
+		mods = "CTRL|SHIFT|ALT",
+		action = wezterm.action.SplitPane({
+			direction = "Right",
+			size = { Percent = 50 },
+		}),
+	},
+	{
+		key = "v",
+		mods = "CTRL|SHIFT|ALT",
+		action = wezterm.action.SplitPane({
+			direction = "Down",
+			size = { Percent = 50 },
+		}),
+	},
+	{
+		key = "U",
+		mods = "CTRL|SHIFT",
+		action = act.AdjustPaneSize({ "Left", 5 }),
+	},
+	{
+		key = "I",
+		mods = "CTRL|SHIFT",
+		action = act.AdjustPaneSize({ "Down", 5 }),
+	},
+	{
+		key = "O",
+		mods = "CTRL|SHIFT",
+		action = act.AdjustPaneSize({ "Up", 5 }),
+	},
+	{
+		key = "P",
+		mods = "CTRL|SHIFT",
+		action = act.AdjustPaneSize({ "Right", 5 }),
+	},
+	{ key = "9", mods = "CTRL", action = act.PaneSelect },
+	{ key = "L", mods = "CTRL", action = act.ShowDebugOverlay },
+	{
+		key = "O",
+		mods = "CTRL|ALT",
+		-- toggling opacity
+		action = wezterm.action_callback(function(window, _)
+			local overrides = window:get_config_overrides() or {}
+			if overrides.window_background_opacity == 1.0 then
+				overrides.window_background_opacity = 0.9
+			else
+				overrides.window_background_opacity = 1.0
+			end
+			window:set_config_overrides(overrides)
+		end),
+	},
 }
 
 -- and finally, return the configuration to wezterm
